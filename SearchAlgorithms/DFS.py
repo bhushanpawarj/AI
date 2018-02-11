1c5e07473c170692e27fbbbd832862afbeae64e1
Bhushan <bhushanpawar@mail.fresnostate.edu>
2018-02-10 16:38:03 -0800

    Initial Commit

---
+++ SearchAlgorithms/DFS.py
@@ -0,0 +1,23 @@
+'''
+Created on Feb 10, 2018
+
+@author: BHUSHAN
+'''
+'''
+Algorithm is as follows :
+-Initial state
+-possible actions in state s 
+-Action behavior->Result
+-Goal test 
+-Path cost
+'''
+
+
+class MyClass(object):
+    '''
+    This is the main class for DFS Algorithm
+    '''
+
+    def __init__(self, params):
+        print("Its a constructor!!")
+        
